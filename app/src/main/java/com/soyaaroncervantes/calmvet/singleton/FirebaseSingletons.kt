b226d6fc8afdbea2631fd7d7b5f9190cd0923da9
Aarón Cervantes <soy.aaron.cervantes@gmail.com>
2021-04-16 23:28:12 -0500

    Added singleton services

---
+++ app/src/main/java/com/soyaaroncervantes/calmvet/singleton/FirebaseSingletons.kt
@@ -0,0 +1,7 @@
+package com.soyaaroncervantes.calmvet.singleton
+
+import com.soyaaroncervantes.calmvet.firebase.FirebaseInstance
+
+object FirebaseSingletons {
+  val firebase = FirebaseInstance.Companion
+}
