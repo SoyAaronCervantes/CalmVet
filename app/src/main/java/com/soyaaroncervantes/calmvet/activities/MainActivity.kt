6a08258221427439c9e44d849910fb5bfa63f3a0
Aarón Cervantes <soy.aaron.cervantes@gmail.com>
2021-04-16 23:31:19 -0500

    Imported singleton firebase instance

--- app/src/main/java/com/soyaaroncervantes/calmvet/activities/MainActivity.kt
+++ app/src/main/java/com/soyaaroncervantes/calmvet/activities/MainActivity.kt
@@ -1,15 +1,10 @@
 package com.soyaaroncervantes.calmvet.activities
 
-import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import com.google.android.gms.auth.api.signin.GoogleSignIn
-import com.google.firebase.auth.FirebaseAuth
-import com.google.firebase.firestore.FirebaseFirestore
-import com.google.firebase.firestore.FirebaseFirestoreSettings
+
 import com.soyaaroncervantes.calmvet.R
-import com.soyaaroncervantes.calmvet.auth.GoogleAuth
-import com.soyaaroncervantes.calmvet.firebase.FirebaseInstance
+import com.soyaaroncervantes.calmvet.singleton.FirebaseSingletons
 
 class MainActivity : AppCompatActivity() {
   override fun onCreate(savedInstanceState: Bundle?) {
@@ -17,9 +12,6 @@ class MainActivity : AppCompatActivity() {
     setContentView(R.layout.activity_main)
 
     // Init Firebase Emulator
-    FirebaseInstance.init();
-    val googleSignInOptions = GoogleAuth.init()
-    val googleAuth = GoogleAuth()
-    googleAuth.googleSignInClient = GoogleSignIn.getClient(this, googleSignInOptions )
+    FirebaseSingletons.firebase
   }
 }
