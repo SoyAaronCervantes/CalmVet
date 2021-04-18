66695a3045ad7b53d0339312c95c9563124bd061
Aarón Cervantes <soy.aaron.cervantes@gmail.com>
2021-04-18 14:05:43 -0500

    Erased Event Logger

--- app/src/main/java/com/soyaaroncervantes/calmvet/facebook/FacebookInstance.kt
+++ app/src/main/java/com/soyaaroncervantes/calmvet/facebook/FacebookInstance.kt
@@ -1,14 +1,11 @@
 package com.soyaaroncervantes.calmvet.facebook
 
-import android.app.Application
 import com.facebook.FacebookSdk
-import com.facebook.appevents.AppEventsLogger
 
 class FacebookInstance {
     companion object {
         init {
             FacebookSdk.fullyInitialize();
-            AppEventsLogger.activateApp( Application() )
         }
     }
 }
