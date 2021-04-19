3b800ce3dc06849c1eabbcf8848605c0b482a370
Aarón Cervantes <soy.aaron.cervantes@gmail.com>
2021-04-18 22:32:50 -0500

    Moved Firebase SignIn to FirebaseInstance class

--- app/src/main/java/com/soyaaroncervantes/calmvet/firebase/FirebaseInstance.kt
+++ app/src/main/java/com/soyaaroncervantes/calmvet/firebase/FirebaseInstance.kt
@@ -1,8 +1,18 @@
 package com.soyaaroncervantes.calmvet.firebase
 
+import android.app.Activity
+import android.content.Context
+import android.content.Intent
+import android.util.Log
+import android.widget.Toast
+import androidx.activity.result.ActivityResult
+import androidx.activity.result.ActivityResultLauncher
 import com.firebase.ui.auth.AuthUI
-import com.google.firebase.firestore.FirebaseFirestore
-import com.google.firebase.firestore.FirebaseFirestoreSettings
+import com.firebase.ui.auth.ErrorCodes
+import com.firebase.ui.auth.IdpResponse
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.FirebaseUser
+import com.soyaaroncervantes.calmvet.R
 
 // Firebase Host Port
 const val HOST_PORT = "10.0.2.2"
@@ -12,17 +22,65 @@ const val FIRESTORE_PORT = 8080
 const val AUTH_PORT = 9099;
 
 class FirebaseInstance {
+  lateinit var firebaseAuth: FirebaseAuth
+  private lateinit var activityResultLauncher: ActivityResultLauncher<Intent>
 
   companion object {
+    lateinit var user: FirebaseUser
+
     init {
       // Create Firebase Firestore instance
       val authUI = AuthUI.getInstance()
-
       // Set Firebase Emulators
       authUI.useEmulator( HOST_PORT, FIRESTORE_PORT );
       authUI.useEmulator( HOST_PORT, AUTH_PORT );
+    }
+  }
+
+  fun launchFirebaseUISignIn( activityResultLauncher: ActivityResultLauncher<Intent> ) {
+
+    this.activityResultLauncher = activityResultLauncher
+
+    val providers = providers()
+    val firebaseIntent = AuthUI.getInstance()
+      .createSignInIntentBuilder()
+      .setAvailableProviders(providers)
+      .setIsSmartLockEnabled(true)
+      .build()
+
+    this.activityResultLauncher.launch(firebaseIntent)
+  }
+  fun validateResult( result: ActivityResult, context: Context ) {
+    val response = IdpResponse.fromResultIntent(result.data)
+
+    // Check if response isn't null
+    if (response === null) { return }
+
+    // Check if user has Network Connection
+    if ( response.error?.errorCode == ErrorCodes.NO_NETWORK ) { Toast.makeText( context, context.getString(R.string.noNetwork), Toast.LENGTH_LONG).show() }
+
+    // Check if user has Network Connection
+    if ( response.error?.errorCode == ErrorCodes.UNKNOWN_ERROR ) { Toast.makeText( context, context.getString(R.string.unknownError), Toast.LENGTH_LONG).show() }
+
+    // If result code is ok, we get currentUser
+
+    if (result.resultCode == Activity.RESULT_OK) {
+
+      val user = firebaseAuth.currentUser
+
+      if ( user !== null ) { FirebaseInstance.user = firebaseAuth.currentUser!! }
 
     }
+
+  }
+  private fun providers(): ArrayList<AuthUI.IdpConfig> {
+    return arrayListOf(
+      AuthUI.IdpConfig.EmailBuilder().build(),
+      AuthUI.IdpConfig.PhoneBuilder().build(),
+      AuthUI.IdpConfig.GoogleBuilder().build(),
+      AuthUI.IdpConfig.FacebookBuilder().build(),
+      AuthUI.IdpConfig.TwitterBuilder().build()
+    )
   }
 
 }
--- app/src/main/java/com/soyaaroncervantes/calmvet/fragments/access/SignInFragment.kt
+++ app/src/main/java/com/soyaaroncervantes/calmvet/fragments/access/SignInFragment.kt
@@ -1,48 +1,25 @@
 package com.soyaaroncervantes.calmvet.fragments.access
 
-import android.app.Activity
 import android.os.Bundle
-import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.Toast
 import androidx.activity.result.ActivityResult
 import androidx.activity.result.contract.ActivityResultContracts
 import androidx.fragment.app.Fragment
-import com.firebase.ui.auth.AuthUI
-import com.firebase.ui.auth.ErrorCodes
-import com.firebase.ui.auth.IdpResponse
 import com.google.firebase.auth.FirebaseAuth
-import com.soyaaroncervantes.calmvet.R
 import com.soyaaroncervantes.calmvet.databinding.FragmentSignInBinding
+import com.soyaaroncervantes.calmvet.firebase.FirebaseInstance
 
 class SignInFragment : Fragment() {
     private lateinit var binding: FragmentSignInBinding
     private lateinit var firebaseAuth: FirebaseAuth
-    private val getContent = registerForActivityResult(ActivityResultContracts.StartActivityForResult()) { result: ActivityResult ->
-        val response = IdpResponse.fromResultIntent(result.data)
-
-        // Check if response isn't null
-        if (response === null) { return@registerForActivityResult }
-
-        // Check if user has Network Connection
-        if ( response.error?.errorCode == ErrorCodes.NO_NETWORK ) { Toast.makeText(requireContext(), getString(R.string.noNetwork), Toast.LENGTH_LONG).show() }
-
-        // Check if user has Network Connection
-        if ( response.error?.errorCode == ErrorCodes.UNKNOWN_ERROR ) { Toast.makeText(requireContext(), getString(R.string.unknownError), Toast.LENGTH_LONG).show() }
-
-        // If result code is ok, we get currentUser
-        if (result.resultCode == Activity.RESULT_OK) {
-            val user = firebaseAuth.currentUser
-
-            if ( user !== null ) {
-                Log.d("[User]", user.uid)
-            }
-        }
+    private lateinit var firebaseInstance: FirebaseInstance
+    private val getContent = registerForActivityResult(ActivityResultContracts.StartActivityForResult()) {
+            result: ActivityResult -> firebaseInstance.validateResult( result, requireContext() )
     }
 
-    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
+    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View {
         binding = FragmentSignInBinding.inflate(inflater, container, false)
         return binding.root
     }
@@ -50,23 +27,9 @@ class SignInFragment : Fragment() {
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
         firebaseAuth = FirebaseAuth.getInstance()
-        val providers = providers()
-        val firebaseIntent = AuthUI.getInstance()
-            .createSignInIntentBuilder()
-            .setAvailableProviders(providers)
-            .setIsSmartLockEnabled(true)
-            .build()
-
-        getContent.launch(firebaseIntent)
+        firebaseInstance = FirebaseInstance()
+        firebaseInstance.firebaseAuth = firebaseAuth
+        firebaseInstance.launchFirebaseUISignIn( getContent )
     }
 
-    private fun providers(): ArrayList<AuthUI.IdpConfig> {
-        return arrayListOf(
-            AuthUI.IdpConfig.EmailBuilder().build(),
-            AuthUI.IdpConfig.PhoneBuilder().build(),
-            AuthUI.IdpConfig.GoogleBuilder().build(),
-            AuthUI.IdpConfig.FacebookBuilder().build(),
-            AuthUI.IdpConfig.TwitterBuilder().build()
-        )
-    }
 }
