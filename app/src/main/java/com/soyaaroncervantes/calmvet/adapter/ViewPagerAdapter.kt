196c37b7d4c318ca5e7edd43728a4ba4d3e8a98b
Aarón Cervantes <soy.aaron.cervantes@gmail.com>
2021-05-04 09:44:20 -0500

    Added tabLayout w/ swipe events

---
+++ app/src/main/java/com/soyaaroncervantes/calmvet/adapter/ViewPagerAdapter.kt
@@ -0,0 +1,20 @@
+package com.soyaaroncervantes.calmvet.adapter
+
+import androidx.fragment.app.Fragment
+import androidx.fragment.app.FragmentActivity
+import androidx.viewpager2.adapter.FragmentStateAdapter
+import com.soyaaroncervantes.calmvet.view.fragments.AddPetFragment
+import com.soyaaroncervantes.calmvet.view.fragments.PetsFragment
+
+class ViewPagerAdapter(private val fragmentActivity: FragmentActivity ): FragmentStateAdapter( fragmentActivity ) {
+  override fun getItemCount(): Int = 2
+
+  override fun createFragment(position: Int): Fragment {
+    return when( position ) {
+      0 -> PetsFragment()
+      1 -> AddPetFragment()
+      else -> PetsFragment()
+    }
+  }
+
+}
--- app/src/main/java/com/soyaaroncervantes/calmvet/view/activities/MainActivity.kt
+++ app/src/main/java/com/soyaaroncervantes/calmvet/view/activities/MainActivity.kt
@@ -5,16 +5,31 @@ import android.os.Bundle
 import android.util.Log
 import android.view.MenuItem
 import android.widget.Toolbar
+import androidx.appcompat.content.res.AppCompatResources
 import androidx.databinding.DataBindingUtil
+import com.google.android.material.tabs.TabLayoutMediator
 
 import com.soyaaroncervantes.calmvet.R
+import com.soyaaroncervantes.calmvet.adapter.ViewPagerAdapter
 import com.soyaaroncervantes.calmvet.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
   private lateinit var binding: ActivityMainBinding
+  private val adapter by lazy { ViewPagerAdapter(this) }
   override fun onCreate(savedInstanceState: Bundle?) {
     super.onCreate(savedInstanceState)
     binding = DataBindingUtil.setContentView( this, R.layout.activity_main )
+    val viewPager = binding.viewPager
+    val tabLayout = binding.tabLayout
+
+    viewPager.adapter = adapter
+    val tabLayoutMediator = TabLayoutMediator( tabLayout, viewPager) { tab, position ->
+      when (position) {
+        0 -> tab.icon = AppCompatResources.getDrawable( this, R.drawable.ic_pets )
+        else -> tab.icon = AppCompatResources.getDrawable( this, R.drawable.ic_add )
+      }
+    }
+    tabLayoutMediator.attach()
   }
 
 }
