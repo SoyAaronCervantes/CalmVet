9032262d1cbc2d9361d75353707e1c87e1550225
Aarón Cervantes <soy.aaron.cervantes@gmail.com>
2021-05-09 02:02:49 -0500

    Modified layout flow

--- app/src/main/java/com/soyaaroncervantes/calmvet/view/fragments/viewpager/ViewPagerFragment.kt
+++ app/src/main/java/com/soyaaroncervantes/calmvet/view/fragments/viewpager/ViewPagerFragment.kt
@@ -1,39 +1,46 @@
 package com.soyaaroncervantes.calmvet.view.fragments.viewpager
 
 import android.os.Bundle
-import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.appcompat.content.res.AppCompatResources
+import androidx.fragment.app.Fragment
 import androidx.viewpager2.widget.ViewPager2
 import com.google.android.material.tabs.TabLayout
 import com.google.android.material.tabs.TabLayoutMediator
 import com.soyaaroncervantes.calmvet.R
 import com.soyaaroncervantes.calmvet.adapter.ViewPagerAdapter
 import com.soyaaroncervantes.calmvet.databinding.FragmentViewPagerBinding
+import com.soyaaroncervantes.calmvet.databinding.TablayoutBinding
 
 class ViewPagerFragment : Fragment() {
   private lateinit var binding: FragmentViewPagerBinding
 
-  override fun onCreateView( inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle? ): View {
+  override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View {
 
-    binding = FragmentViewPagerBinding.inflate( inflater, container, false )
-    val fragmentList = arrayListOf( PetsFragment(), FavoritePetsFragment() )
-    val adapter = ViewPagerAdapter( fragmentList, childFragmentManager, lifecycle )
-    val viewPager =  binding.viewPager
+    binding = FragmentViewPagerBinding.inflate(inflater, container, false)
+    val view = binding.root
+    val fragmentList = arrayListOf(PetsFragment(), FavoritePetsFragment())
+    val adapter = ViewPagerAdapter(fragmentList, childFragmentManager, lifecycle)
+
+    val viewPager = binding.viewPager
+    val tabBar = TablayoutBinding.bind( view )
+
     viewPager.adapter = adapter
-    tabLayoutManager( binding.tabLayout, viewPager )
 
-    return binding.root
+    tabLayoutManager(tabBar.tabLayout, viewPager)
+
+    return view
   }
 
-  private fun tabLayoutManager( tabLayout: TabLayout, viewPager: ViewPager2 ) {
+  private fun tabLayoutManager(tabLayout: TabLayout, viewPager: ViewPager2) {
 
-    val tabLayoutMediator = TabLayoutMediator( tabLayout, viewPager) { tab, position ->
+    val tabLayoutMediator = TabLayoutMediator(tabLayout, viewPager) { tab, position ->
       when (position) {
-        0 -> tab.icon = AppCompatResources.getDrawable( requireContext(), R.drawable.ic_pets )
-        else -> tab.icon = AppCompatResources.getDrawable( requireContext(), R.drawable.ic_add )
+        0 -> tab.icon = AppCompatResources.getDrawable(requireContext(), R.drawable.ic_pets)
+        1 -> tab.icon = AppCompatResources.getDrawable(requireContext(), R.drawable.ic_favorite )
+        else -> throw Throwable("Error on TabLayout, position: $position")
       }
     }
 
