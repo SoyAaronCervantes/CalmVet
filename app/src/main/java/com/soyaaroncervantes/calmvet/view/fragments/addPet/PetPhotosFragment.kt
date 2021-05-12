04de5abe53f63ea14c32eca1a35fa6e9908b7122
Aarón Cervantes <soy.aaron.cervantes@gmail.com>
2021-05-12 00:31:30 -0500

    Fixed issues when we get an image from content

--- app/src/main/java/com/soyaaroncervantes/calmvet/view/fragments/addPet/PetPhotosFragment.kt
+++ app/src/main/java/com/soyaaroncervantes/calmvet/view/fragments/addPet/PetPhotosFragment.kt
@@ -2,19 +2,25 @@ package com.soyaaroncervantes.calmvet.view.fragments.addPet
 
 import android.Manifest
 import android.content.pm.PackageManager
+import android.icu.text.SimpleDateFormat
 import android.os.Bundle
+import android.os.Environment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.Toast
 import androidx.activity.result.contract.ActivityResultContracts
 import androidx.core.content.ContextCompat
+import androidx.core.net.toUri
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.activityViewModels
+import com.bumptech.glide.Glide
 import com.soyaaroncervantes.calmvet.databinding.FragmentPetPhotosBinding
 import com.soyaaroncervantes.calmvet.models.pets.Animal
 import com.soyaaroncervantes.calmvet.viewmodel.PetViewModel
 import com.soyaaroncervantes.calmvet.viewmodel.ToolbarViewModel
+import java.io.File
+import java.util.*
 
 class PetPhotosFragment : Fragment() {
   private lateinit var binding: FragmentPetPhotosBinding
@@ -22,22 +28,26 @@ class PetPhotosFragment : Fragment() {
   private val petViewModel: PetViewModel by activityViewModels()
   private val toolBarViewModel: ToolbarViewModel by activityViewModels()
 
-  private val requestTakePictureLauncher = registerForActivityResult(ActivityResultContracts.TakePicturePreview() ) { bitmap ->
-    val previewView = binding.previewViewCamera
-    
+  private fun hasCameraPermissions() = ContextCompat.checkSelfPermission(requireContext(), Manifest.permission.CAMERA) == PackageManager.PERMISSION_GRANTED
+  private fun hasGalleryPermissions() = ContextCompat.checkSelfPermission(requireContext(), Manifest.permission.READ_EXTERNAL_STORAGE) == PackageManager.PERMISSION_GRANTED
+  private fun hasWritePermissions() = ContextCompat.checkSelfPermission(requireContext(), Manifest.permission.WRITE_EXTERNAL_STORAGE) == PackageManager.PERMISSION_GRANTED
+
+  private val requestTakePictureLauncher = registerForActivityResult(ActivityResultContracts.TakePicturePreview()) {
+
   }
 
-  private val requestPickPictureLauncher = registerForActivityResult( ActivityResultContracts.GetContent() ) {
-
+  private val requestPickPictureLauncher = registerForActivityResult(ActivityResultContracts.GetContent()) { photo ->
+    Glide
+      .with(this)
+      .load(photo)
+      .fitCenter()
+      .into(binding.imageViewProfile)
   }
 
   override fun onResume() {
     super.onResume()
     toolBarViewModel.setTitle("Agregar Fotos");
   }
-
-  private fun hasCameraPermissions() = ContextCompat.checkSelfPermission(requireContext(), Manifest.permission.CAMERA) == PackageManager.PERMISSION_GRANTED
-  private fun hasGalleryPermissions() = ContextCompat.checkSelfPermission(requireContext(), Manifest.permission.READ_EXTERNAL_STORAGE) == PackageManager.PERMISSION_GRANTED
 
   override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
     binding = FragmentPetPhotosBinding.inflate(inflater, container, false)
@@ -51,27 +61,45 @@ class PetPhotosFragment : Fragment() {
     val galleryButton = binding.galleryButton
 
     cameraButton.setOnClickListener {
-      val cameraPermission = hasCameraPermissions()
+      hasCameraPermissions()
+      hasWritePermissions()
       launchCamera()
     }
 
     galleryButton.setOnClickListener {
-      val galleryPermission = hasGalleryPermissions()
+      hasGalleryPermissions()
       launchGallery()
     }
   }
 
-  private fun takePhoto() {
-    showToast("Take photo here")
-  }
+
   private fun selectPhoto() {
     showToast("Pick a photo here")
   }
 
-  private fun launchCamera() {  } // <- Launch Camera
+  // Launch Camera
+  private fun launchCamera() {
+    val currentPhotoPath = createImageFile()
+    val photoUri = currentPhotoPath.absoluteFile.toUri()
+
+    requestTakePictureLauncher.launch(photoUri)
+
+  }
+
   private fun launchGallery() {
     requestPickPictureLauncher.launch("image/*")
-  } // <- Launch Gallery
+  }
+
+
+  private fun createImageFile(): File {
+    val timeStamp = SimpleDateFormat("yyyy-MM:dd-HH:mm:ss", Locale.ROOT).format(Date())
+    val storageDir: File? = context?.getExternalFilesDir(Environment.DIRECTORY_PICTURES);
+    return File.createTempFile(
+      "JPEG_${timeStamp}_",
+      "jpg",
+      storageDir
+    );
+  }
 
   // #Toast
   private fun showToast(message: String) {
