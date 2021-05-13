d2904bae7cf728726046ce830810d4b6f67e6fab
Aarón Cervantes <soy.aaron.cervantes@gmail.com>
2021-05-13 00:30:53 -0500

    MOdified design, and added new function on viewmodel

--- app/src/main/java/com/soyaaroncervantes/calmvet/viewmodel/PetViewModel.kt
+++ app/src/main/java/com/soyaaroncervantes/calmvet/viewmodel/PetViewModel.kt
@@ -3,6 +3,7 @@ package com.soyaaroncervantes.calmvet.viewmodel
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
+import com.google.firebase.firestore.FirebaseFirestore
 import com.soyaaroncervantes.calmvet.models.pets.Animal
 
 class PetViewModel: ViewModel() {
@@ -16,5 +17,15 @@ class PetViewModel: ViewModel() {
 
   fun getAnimalAvailable(): ArrayList<String> {
     return arrayListOf( "Perro", "Gato", "Cuyo", "Hurón", "Conejo", "Pato" )
+  }
+
+  fun save(animal: Animal) {
+    val firestore = FirebaseFirestore.getInstance()
+    val ref = firestore.collection(PETS_COLLECTION).document( animal.id )
+
+  }
+
+  companion object {
+    private const val PETS_COLLECTION = "pets"
   }
 }
